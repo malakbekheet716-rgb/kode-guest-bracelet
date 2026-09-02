aca4a1025d235bb8e14d0dfbc7ca8852df5e4458
Malak Mohamed <malakbekheet716@gmail.com>
2026-09-02 15:43:45 +0300

    Apply PDF branding: purple KODE wordmark, extracted K logos, and brand color palette

--- src/components/ui/BrandLockup.jsx
+++ src/components/ui/BrandLockup.jsx
@@ -1,13 +1,14 @@
 import { useTheme } from '../../context/ThemeContext';
-import kMarkBlack from '../../assets/kode-k-mark-black.png';
-import kMarkWhite from '../../assets/kode-k-mark-white.png';
+import kLogo from '../../assets/kode-k-logo.png';
+import kLogoDark from '../../assets/kode-k-logo-dark.png';
+import kodeWordmark from '../../assets/kode-wordmark-purple.png';
 
 export default function BrandLockup({ size = 'md' }) {
   const { isDark } = useTheme();
   return (
     <div className={`brand-lockup brand-lockup--${size}`}>
-      <img src={isDark ? kMarkWhite : kMarkBlack} alt="" className="brand-lockup__icon" />
-      <span className="brand-lockup__word">KODE</span>
+      <img src={isDark ? kLogoDark : kLogo} alt="" className="brand-lockup__icon" />
+      <img src={kodeWordmark} alt="KODE" className="brand-lockup__wordmark" />
     </div>
   );
 }
